c370de2c069f3023b16134c57807072f883cec23
Emily Liu <liuemily@uw.edu>
2019-01-30 14:01:13 -0800

    work on exercise 1

--- chapter-11-exercises/exercise-1/exercise.R
+++ chapter-11-exercises/exercise-1/exercise.R
@@ -20,36 +20,42 @@ manufacturers <- c(vehicles$make)
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
 #unique(vehicles, manufacturers, fromLast = TRUE)
-
+?unique()
+unique(manufacturers)
 
 # Filter the data set for vehicles manufactured in 1997
-vehicles[vehicles$year == 1997]
+cars_1997 <- vehicles[vehicles$year == 1997, ]
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
 # See also:
 # https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
-order(vehicles[vehicles$year == 1997], hwy)
+order(cars_1997, hwy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+cars_1997["average"] <- (cty + mpg)/2
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+cty_mileage <- vehicles$cty > 20
+2wd_df <- vehicles[vehicles$drive == "2-Wheel Drive", cty_mileage]
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
+vehicles[vehicles$hwy == min(vehicles$hwy), id]
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters, 
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
-
-
+mileage_year <- function(year_choice, make_choice) {
+  make <- vehicles[vehicles$year == year_choice |
+          vehicles$make == make_choice, ]
+  model <- make[max(make$hwy), "model"]
+}
+paste(mileage_year(1995, "Honda"))
 # What was the most efficient Honda model of 1995?
 
