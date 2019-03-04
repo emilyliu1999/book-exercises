f84acfc9c4c9440149152164673a45237a05c59c
Emily Liu <liuemily@uw.edu>
2019-03-04 14:44:47 -0800

    finish exercise 3 in class

--- chapter-19-exercises/exercise-3/app.R
+++ chapter-19-exercises/exercise-3/app.R
@@ -20,47 +20,68 @@ select_values <- colnames(diamonds)
 
 
 # Define a UI using a `fluidPage()` layout with the following content:
-
+ui <- fluidPage(
   
   # A `titlePanel` with the title "Diamond Viewer"
-
+  titlePanel("Diamond Viewer"),
   
   # A `sliderInput()` labeled "Price (in dollars)". This slider should let the 
   # user pick a range between the minimum and maximum price of the entire 
   # diamond data set. The initial value should be the vector of min/max price.
-
+  sliderInput(
+    label = "Price (in dollars)",
+    inputId = "price",
+    min = price_range[1],
+    max = price_range[2],
+    value = price_range
+  ),
   
   # A `selectInput()` labeled "Feature of Interest". This dropdown should let
   # the user pick one of the columns of the diamond data set. Use the `carat`
   # column as a default
-
+  selectInput(
+    label = "Feature of Interest",
+    inputId = "feature",
+    choices = select_values,
+    selected = "carat"
+  ),
 
   # A `checkboxInput()` labeled "Show Trendline". It's default value is TRUE
-
+  checkboxInput(
+    label = "Show Trendline",
+    inputId = "has_trendline",
+    value = TRUE
+  ),
   
   # A plotOutput showing the 'plot' output (based on the user specifications)
+  plotOutput(
+    outputId = "my_plot"),
 
-  
   # Bonus: a dataTableOutput showing a data table of relevant observations
-
+  dataTableOutput(outputId = "my_data_table")
+) # end of UI
 
 
 # Define a `server` function (with appropriate arguments)
 # This function should perform the following:
-
+server <- function(input, output) {
 
   # Assign a reactive `renderPlot()` function to the outputted 'plot' value
-
+  output$my_plot <- renderPlot({
     
     # This function should take the `diamonds_sample` data set and filter it by 
     # the input price (remember to get both ends)!
-
+    filtered_diamonds <- diamonds_sample %>% 
+      filter(price > input$price[1] & price < input$price[2])
+    
     # Use the filtered data set to create a ggplot2 scatter plot with the carat 
     # on the x-axis, the price on the y-axis, and color based on the clarity. 
     # Facet the plot based on which feature the user selected to "facet by"
     #   (hint: you can just pass that string directly to `facet_wrap()`)
     # Save your plot as a variable.
-
+    p <- ggplot(data = filtered_diamonds) +
+      geom_point(mapping = aes_string(x = input$feature, y = "price", color = "clarity"))
+    p
     
     # Finally, if the "trendline" checkbox is selected, you should also include 
     # a geom_smooth geometry (with `se=FALSE`)
@@ -68,10 +89,12 @@ select_values <- colnames(diamonds)
     # Be sure and return the completed plot!
 
     
+  }) # end of renderPlot
+} # end of server
 
 
 # Create a new `shinyApp()` using the above ui and server
-
+shinyApp(ui, server)
 
 ## Double Bonus: For fun, can you make a similar browser for the `mpg` data set?
 ## it makes the bonus data table a lot more useful!
